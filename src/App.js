eaa8822da3aa692b71b28cda6ecc1713e2864e72
Mangeshi <thisismangeshi@gmail.com>
2021-05-10 18:49:19 +0530

    Added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import "./App.css"
+import "./App.css";
+import NavBar from "./components/navbar/nav";
 
 function App() {
-    return(
-    <>
-        <section>
-            <div className="wave water wave1"></div>
-            <div className="wave water wave2"></div>
-            <div className="wave water wave3"></div>
-        </section>
-    </>
-    )
+    return (
+        <>
+            <section>
+                <NavBar />
+
+                <div>
+                    <div className="wave water wave1"></div>
+                    <div className="wave water wave2"></div>
+                    <div className="wave water wave3"></div>
+                </div>
+            </section>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/navbar/nav.jsx
@@ -0,0 +1,19 @@
+import "./nav.css"
+
+const NavBar = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Pages</li>
+                    <li>Services</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </>
+    );
+};
+
+export default NavBar
